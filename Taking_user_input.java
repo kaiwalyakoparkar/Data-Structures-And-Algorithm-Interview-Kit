16061b9ee5ab97d3dc9b362573a7a1e40a2c1ef5
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-19 20:01:41 +0500

    Took input as int, float and string

--- Taking_user_input.java
+++ Taking_user_input.java
@@ -7,8 +7,21 @@ public class Taking_user_input {
         //using scanner as a datatype as sc as a name
         System.out.println("Enter Your Age : ");
         Scanner sc = new Scanner(System.in);//This tells compiler that it is going to store the input 
-        int age = sc.nextInt();//This initiates the input method
-        System.out.println("Your age is :");
+        int age = sc.nextInt(); 
+        //This initiates the input method
+        //Also it signifes that the age is integer
+         System.out.println("Your age is :");
         System.out.println(age);//printing the taken input
+
+        System.out.println("Enter your marks : ");
+        float marks = sc.nextFloat();
+        System.out.println("Your marks are :");
+        System.out.println(marks);
+
+        System.out.println("Enter your name");
+        String name = sc.nextLine(); 
+        System.out.println("Entered Name is : ");
+        System.out.println(name);
+       
     }
 }
