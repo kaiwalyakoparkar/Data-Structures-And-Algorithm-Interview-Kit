7ff2d9df76e7586b061828f475aca9e5c3b11555
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-19 19:34:59 +0500

    Operators are added to the folder!

---
+++ Operators.java
@@ -0,0 +1,8 @@
+public class Operators{
+	public static class main(string[] args){
+		int firstNumber = 13;
+		int secondNumber = 15;
+		int sum = firstNumber + secondNumber;
+		System.out.println(sum);
+	}
+}
