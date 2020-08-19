e6ea04ea364acf0f34d22801334c5477128d2d41
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-19 19:40:19 +0500

    Type cast added!

--- Operators.java
+++ Operators.java
@@ -3,6 +3,11 @@ public class Operators{
 		int firstNumber = 13;
 		int secondNumber = 15;
 		int sum = firstNumber + secondNumber;
-		System.out.println(sum);
+        System.out.println(sum);
+        /*
+        double sum = (double)firstNumber + (double)secondNumber;
+
+        In this the int data type is type casted to doule datatype
+         */
 	}
 }
