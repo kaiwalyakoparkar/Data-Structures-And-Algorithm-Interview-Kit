7ee48f9f1a7f3790c9f1c0ab130aa240746c0e07
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-20 20:28:24 +0500

    Exception handled gracefullygit add .

---
+++ Exception_handling.java
@@ -0,0 +1,16 @@
+
+public class Exception_handling{
+
+    public static void main(String[] args){
+
+        int a[] = new int[3];
+
+        try{
+            System.out.println(2/0);//This will cause error
+        }catch(Exception e){
+            //This will ignore the error and will only report us that error had occured.
+            System.out.println("Error occured here!");
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+}
