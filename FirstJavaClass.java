f15eac3a979cbb745f2f7c63056d94c9529c2fef
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-21 07:33:12 +0500

    Finally did it

--- FirstJavaClass.java
+++
@@ -1,8 +0,0 @@
-
-public class FirstJavaClass {
-
-    public static void main(String[] args) {
-    
-        System.out.println("Hello World !");
-    }
-}
--- Matrix.java
+++
@@ -1,9 +0,0 @@
-public class Matrix{
-
-    public static void main(String[] args){
-
-        int a[][]={{1,2},{3,4}};
-
-        System.out.println(a[0][0]);
-    }
-}
