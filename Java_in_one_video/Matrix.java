4a7138038860ae8bea57ca0e8daa6aee4998681d
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-21 19:00:27 +0500

    Lecture program for ds algo from apni kaksha

---
+++ Java_in_one_video/Matrix.java
@@ -0,0 +1,9 @@
+public class Matrix{
+
+    public static void main(String[] args){
+
+        int a[][]={{1,2},{3,4}};
+
+        System.out.println(a[0][0]);
+    }
+}
---
+++ Java_in_one_video/Operators.java
@@ -0,0 +1,13 @@
+public class Operators{
+	public static class main(string[] args){
+		int firstNumber = 13;
+		int secondNumber = 15;
+		int sum = firstNumber + secondNumber;
+        System.out.println(sum);
+        /*
+        double sum = (double)firstNumber + (double)secondNumber;
+
+        In this the int data type is type casted to doule datatype
+         */
+	}
+}
