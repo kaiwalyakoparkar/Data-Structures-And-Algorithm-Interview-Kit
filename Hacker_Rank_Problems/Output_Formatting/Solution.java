8b9a7d4edeb9f35dcae602072724f1290200176b
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-08-22 06:44:34 +0500

    Fine try

--- Hacker_Rank_Problems/Output_Formatting/Solution.java
+++ Hacker_Rank_Problems/Output_Formatting/Solution.java
@@ -6,9 +6,9 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         System.out.println("================================");
         for (int i = 0; i < 3; i++) {
-            String s1 = sc.nextLine();
+            String s1 = sc.next();
             int x = sc.nextInt();
-
+            System.out.printf("%-15s%03d %n", s1, x);
         }
         System.out.println("================================");
 
