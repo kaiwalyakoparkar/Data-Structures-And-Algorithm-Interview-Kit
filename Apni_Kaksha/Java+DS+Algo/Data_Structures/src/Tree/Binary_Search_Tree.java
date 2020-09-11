7e3faa841b667613fa8298d5ed261420cbd8516a
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-11 17:09:17 +0530

    Creating own binary tree with all the methods of traversing using recursion

--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
@@ -3,41 +3,58 @@ import java.util.*;
 
 public class Binary_Search_Tree {
 	
-	public class Tree{
+	/*Node class same as Linked List*/
+	public class Node{
+		int data;
+		Node right;
+		Node left;
 		
-		public class Node{
-			Node left;
-			Node right;
-			int data;
-			
-			public Node(int data) {
-				this.data = data;
-			}
-			
-			void addNode(int data) {
-				int size=0;
-				if(size==0) {
-					Node root = new Node(data);
-					root.left = root.right = null;
-				}else{
-					Node a = new Node(data);
-					if(a.data < data) {
-						left = a;
-						a.left=a.right=null;
-						root=a;
-					}
-				}
-				size++;
-			}
+		Node(int data){
+			this.data=data;
 		}
 	}
+	/*Node class finishes here*/
+	static Node createTree(){
+		
+		Node root = null;//keep root node null initially
+		System.out.println("Enter the data: ");
+		
+		//receive the data
+		int data = sc.nextInt();
+		
+		//check if the data is null;
+		if(data == -1) {
+			return null;
+		}
+		
+		//Add the data to root node
+		root = new Node(data);
+		
+		//take value of left node
+		System.out.println("Enter left for "+data);
+		
+		/*Recursion for left node starts here
+		 *when it will receive the -1 means the null
+		 *value that time it will come back and
+		 *take the value of right*/
+		root.left = createTree();
+		
+		System.out.println("Enter right for "+data);
+		/*Recursion for right node starts here
+		 *when it will receive the -1 means the null
+		 *value that time it will come back to
+		 *root node*/
+		root.right = createTree();
+		return root;
+	}
+	
+	
+	/**/
 	
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int data = sc.nextInt();
-		sc.close();
-		
 		
 	}
+	
+	
+	
 }
