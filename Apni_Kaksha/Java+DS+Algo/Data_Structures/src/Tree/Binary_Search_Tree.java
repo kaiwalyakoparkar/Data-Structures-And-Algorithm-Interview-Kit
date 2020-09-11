0c34192bfb6d9520cc6806ba2e87916e7e9295c1
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-11 18:33:17 +0530

    Completed the binary tree with all the tree types of traversals as well as used recursion to carry out its tasks

--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Binary_Search_Tree {
 	
+	static Scanner sc = null;
 	/*Node class same as Linked List*/
 	public class Node{
 		int data;
@@ -48,11 +49,48 @@ public class Binary_Search_Tree {
 		return root;
 	}
 	
+	/*This is for InOrder Traversal*/
+	static void inOrder(Node root) {
+		if(root == null) return;
+		
+		/*This too uses recursion to show
+		 *the tree*/
+		inOrder(root.left);
+		System.out.print(root.data+" ");
+		inOrder(root.right);
+	}
 	
-	/**/
+	
+	/*This is for PreOrder Traversal*/
+	static void preOrder(Node root) {
+		if(root == null) return;
+		
+		/*This too uses recursion to show
+		 *the tree*/
+		System.out.println(root.data+" ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+	
+	
+	/*This is for PostOrder Traversal*/
+	static void postOrder(Node root) {
+		if(root == null) return;
+		
+		/*This too uses recursion to show 
+		 *the tree*/
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.println(root.data+" ");
+	}
+	
+	
+	/*This is the main function*/
 	
 	public static void main(String[] args) {
+		sc = new Scanner(System.in);
 		
+		Node root = createTree();
 	}
 	
 	
