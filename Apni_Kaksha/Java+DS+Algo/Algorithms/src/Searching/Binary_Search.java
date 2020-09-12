d606ee75e4c7e7ac86cc417b3a67283084e98ef2
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-12 09:12:33 +0530

    Binary search added

---
+++ Apni_Kaksha/Java+DS+Algo/Algorithms/src/Searching/Binary_Search.java
@@ -0,0 +1,12 @@
+package Searching;
+import java.util.*;
+
+public class Binary_Search {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+	}
+}
