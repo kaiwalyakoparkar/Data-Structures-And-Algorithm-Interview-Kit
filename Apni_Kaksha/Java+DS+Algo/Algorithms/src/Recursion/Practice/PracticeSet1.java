d4c2150d7869ace620fb9cc5b36d9ef83ae778ff
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-17 16:28:52 +0530

    Recursion practice 1

--- Apni_Kaksha/Java+DS+Algo/Algorithms/src/Recursion/Practice/PracticeSet1.java
+++ Apni_Kaksha/Java+DS+Algo/Algorithms/src/Recursion/Practice/PracticeSet1.java
@@ -6,15 +6,20 @@
  * 
  */
 package Recursion.Practice;
+import java.util.*;
 
 public class PracticeSet1 {
 
 	public static void main(String[] args) {
 		
-		
-		
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		sc.close();
+		pract1(n);
 	}
-	public static void pract1() {
-		
+	public static void pract1(int n){
+		if(n==1) {
+			return 1;
+		}
 	}
 }
