57e18a04451a45e405960648c5a6d00b874115ba
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-20 18:19:17 +0530

    Changed the implimentation from array list to the linked list which is generally asked

--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Graphs/AdjacencyList.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Graphs/AdjacencyList.java
@@ -1,59 +1,53 @@
 package Graphs;
 
 //Java code to demonstrate Graph representation 
-//using ArrayList in Java 
+//using Linked List in Java 
 //Following is the Adjacency list
 
-import java.util.*; 
 
-class AdjacencyList { 
-			// A utility function to add an edge in an 
-			// undirected graph 
-			static void addEdge(ArrayList<ArrayList<Integer> > adj, 
-								int u, int v) 
-			{ 
-				adj.get(u).add(v); 
-				adj.get(v).add(u); 
-			} 
-
-			// A utility function to print the adjacency list 
-			// representation of graph 
-			static void printGraph(ArrayList<ArrayList<Integer> > adj) 
-			{ 
-				for (int i = 0; i < adj.size(); i++) { 
-					System.out.println("\nAdjacency list of vertex" + i); 
-					System.out.print("head"); 
-					for (int j = 0; j < adj.get(i).size(); j++) { 
-						System.out.print(" -> "+adj.get(i).get(j)); 
-					} 
-					System.out.println(); 
-				} 
-			} 
-	
-	// Driver Code 
-	public static void main(String[] args) 
-	{ 
-		// Creating a graph with 5 vertices 
-		int V = 5; 
-		ArrayList<ArrayList<Integer> > adj 
-					= new ArrayList<ArrayList<Integer> >(V); 
+import java.util.*;
+class Graph{
+	class Edge{
+		int v,w;
+		public Edge(int v,int w){
+			this.v=v; this.w=w;
+		}
+		@Override
+		public String toString(){
+			return "("+v+","+w+")";
+		}
+	}
+	List<Edge> G[];
+	public Graph(int n){
+		G=new LinkedList[n];
+		for(int i=0;i<G.length;i++)
+			G[i]=new LinkedList<Edge>();
+	}
+	boolean isConnected(int u,int v){
+		for(Edge i: G[u])
+			if(i.v==v) return true;
+		return false;
+	}
+	void addEdge(int u,int v,int w){
+		G[u].add(0,new Edge(v,w)); 
+	}
+	@Override
+	public String toString(){
+		String result="";
+		for(int i=0;i<G.length;i++)
+			result+=i+"=>"+G[i]+"\n";
+		return result;
+	}
+}
+public class AdjacencyList {
+	public static void main(String[] args) {
+		Graph g=new Graph(10);
+		g.addEdge(0, 2, 10);
+		g.addEdge(0, 5, 15);
+		g.addEdge(2, 5, 10);
+		g.addEdge(9, 3, 16);
 		
-		for (int i = 0; i < V; i++) 
-			adj.add(new ArrayList<Integer>()); 
-
-		// Adding edges one by one 
-		addEdge(adj, 0, 1); 
-		addEdge(adj, 0, 4); 
-		addEdge(adj, 1, 2); 
-		addEdge(adj, 1, 3); 
-		addEdge(adj, 1, 4); 
-		addEdge(adj, 2, 3); 
-		addEdge(adj, 3, 4); 
-		
-		printGraph(adj); 
-	} 
-		
-
-} 
-
-
+		System.out.println(g);
+		System.out.println(g.isConnected(9,3));
+	}
+}
