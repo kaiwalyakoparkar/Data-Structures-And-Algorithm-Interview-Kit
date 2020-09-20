9cc5130079f0cc270e3d1ef007b6f4d6c6dfaf42
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-20 17:30:05 +0530

    Allignment changed for the code all the methods are shifted upwards and the main method is pushed downwords

--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Graphs/AdjacencyList.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Graphs/AdjacencyList.java
@@ -7,6 +7,28 @@ package Graphs;
 import java.util.*; 
 
 class AdjacencyList { 
+			// A utility function to add an edge in an 
+			// undirected graph 
+			static void addEdge(ArrayList<ArrayList<Integer> > adj, 
+								int u, int v) 
+			{ 
+				adj.get(u).add(v); 
+				adj.get(v).add(u); 
+			} 
+
+			// A utility function to print the adjacency list 
+			// representation of graph 
+			static void printGraph(ArrayList<ArrayList<Integer> > adj) 
+			{ 
+				for (int i = 0; i < adj.size(); i++) { 
+					System.out.println("\nAdjacency list of vertex" + i); 
+					System.out.print("head"); 
+					for (int j = 0; j < adj.get(i).size(); j++) { 
+						System.out.print(" -> "+adj.get(i).get(j)); 
+					} 
+					System.out.println(); 
+				} 
+			} 
 	
 	// Driver Code 
 	public static void main(String[] args) 
@@ -30,28 +52,7 @@ class AdjacencyList {
 		
 		printGraph(adj); 
 	} 
-	// A utility function to add an edge in an 
-		// undirected graph 
-		static void addEdge(ArrayList<ArrayList<Integer> > adj, 
-							int u, int v) 
-		{ 
-			adj.get(u).add(v); 
-			adj.get(v).add(u); 
-		} 
-
-		// A utility function to print the adjacency list 
-		// representation of graph 
-		static void printGraph(ArrayList<ArrayList<Integer> > adj) 
-		{ 
-			for (int i = 0; i < adj.size(); i++) { 
-				System.out.println("\nAdjacency list of vertex" + i); 
-				System.out.print("head"); 
-				for (int j = 0; j < adj.get(i).size(); j++) { 
-					System.out.print(" -> "+adj.get(i).get(j)); 
-				} 
-				System.out.println(); 
-			} 
-		} 
+		
 
 } 
 
