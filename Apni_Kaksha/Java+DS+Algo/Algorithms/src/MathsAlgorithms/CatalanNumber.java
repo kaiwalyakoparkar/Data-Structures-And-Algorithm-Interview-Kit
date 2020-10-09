cb0ec1027bf2b3ba3e80a9b62082332174d880d7
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-09 11:58:51 +0530

    Catalan number generated successfully

---
+++ Apni_Kaksha/Java+DS+Algo/Algorithms/src/MathsAlgorithms/CatalanNumber.java
@@ -0,0 +1,18 @@
+//Showing the catalan number on the nth postion
+package MathsAlgorithms;
+import java.util.*;
+public class CatalanNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1] =1;
+        for(int i=2; i<dp.length; i++){
+            for(int j=0 ;j<i; j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
