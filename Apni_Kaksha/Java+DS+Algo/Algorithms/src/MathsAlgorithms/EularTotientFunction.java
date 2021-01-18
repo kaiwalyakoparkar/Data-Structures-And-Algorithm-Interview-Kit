a0dff5549d3a6aa9d4f4acb85eee704456d7cf5d
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2021-01-18 18:29:35 +0530

    Unpushed files are pushed

--- Apni_Kaksha/Java+DS+Algo/Algorithms/src/MathsAlgorithms/EularTotientFunction.java
+++ Apni_Kaksha/Java+DS+Algo/Algorithms/src/MathsAlgorithms/EularTotientFunction.java
@@ -1,2 +1,31 @@
-package MathsAlgorithms;public class EularTotientFunction {
+//This program is used to find the the answer of phi(n)
+package MathsAlgorithms;
+import java.util.*;
+public class EularTotientFunction {
+    static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    // A simple method to evaluate
+    // Euler Totient Function
+    static int phi(int n)
+    {
+        int result = 1;
+        for (int i = 2; i < n; i++){
+            if (gcd(i, n) == 1){
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("phi(" + n + ") = " + phi(n));
+    }
 }
