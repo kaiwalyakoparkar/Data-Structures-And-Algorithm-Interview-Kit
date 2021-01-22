3f54047ffbf4a6dcd9167acf57d3892781339b35
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2021-01-22 12:56:01 +0530

    Tree and Linked list changes

--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Linked_List/Get_value_of_index.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Linked_List/Get_value_of_index.java
@@ -7,13 +7,13 @@ public class Get_value_of_index {
 		int data;
 		Node next;
 	}
-	
+
 	public static class LinkedList{
-		
+
 		Node head;
 		Node tail;
 		int size;
-		
+
 		public void add(int val) {
 			Node n = new Node();
 			n.data = val;
@@ -25,10 +25,10 @@ public class Get_value_of_index {
 				tail=n;
 			}
 		}
-		
+
 		public void getInd(int index) {
 			int length=1;
-			if(size+1<=index) {
+			if(size+1>=index) {
 				System.out.println("Index out of bound!");
 			}else {
 				for(Node temp=head; temp!=null; temp=temp.next) {
@@ -40,24 +40,24 @@ public class Get_value_of_index {
 					}
 				}
 			}
-			
-			
+
+
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many elements are there in node:");
 		int times = sc.nextInt();
-		
+
 		System.out.println("Enter "+times+" element in LinkedList");
 		for(int i=0; i<times; i++) {
 			int data = sc.nextInt();
 			list.add(data);
 		}
-		
+
 		System.out.println("Enter the index of the node:");
 		int index = sc.nextInt();
 		list.getInd(index);
--- Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
+++ Apni_Kaksha/Java+DS+Algo/Data_Structures/src/Tree/Binary_Search_Tree.java
@@ -2,44 +2,44 @@ package Tree;
 import java.util.*;
 
 public class Binary_Search_Tree {
-	
+
 	static Scanner sc = null;
 	/*Node class same as Linked List*/
-	public class Node{
+	public static class Node{
 		int data;
 		Node right;
 		Node left;
-		
+
 		Node(int data){
 			this.data=data;
 		}
 	}
 	/*Node class finishes here*/
-	static Node createTree(){
-		
+	public static Node createTree(){
+
 		Node root = null;//keep root node null initially
 		System.out.println("Enter the data: ");
-		
+
 		//receive the data
 		int data = sc.nextInt();
-		
+
 		//check if the data is null;
 		if(data == -1) {
 			return null;
 		}
-		
+
 		//Add the data to root node
 		root = new Node(data);
-		
+
 		//take value of left node
 		System.out.println("Enter left for "+data);
-		
+
 		/*Recursion for left node starts here
 		 *when it will receive the -1 means the null
 		 *value that time it will come back and
 		 *take the value of right*/
 		root.left = createTree();
-		
+
 		System.out.println("Enter right for "+data);
 		/*Recursion for right node starts here
 		 *when it will receive the -1 means the null
@@ -47,52 +47,60 @@ public class Binary_Search_Tree {
 		 *root node*/
 		root.right = createTree();
 		return root;
+
 	}
-	
+
 	/*This is for InOrder Traversal*/
-	static void inOrder(Node root) {
+	public static void inOrder(Node root) {
 		if(root == null) return;
-		
+
 		/*This too uses recursion to show
 		 *the tree*/
 		inOrder(root.left);
 		System.out.print(root.data+" ");
 		inOrder(root.right);
 	}
-	
-	
+
+
 	/*This is for PreOrder Traversal*/
-	static void preOrder(Node root) {
+	public static void preOrder(Node root) {
 		if(root == null) return;
-		
+
 		/*This too uses recursion to show
 		 *the tree*/
-		System.out.println(root.data+" ");
+		System.out.print(root.data+" ");
 		preOrder(root.left);
 		preOrder(root.right);
 	}
-	
-	
+
+
 	/*This is for PostOrder Traversal*/
-	static void postOrder(Node root) {
+	public static void postOrder(Node root) {
 		if(root == null) return;
-		
-		/*This too uses recursion to show 
+
+		/*This too uses recursion to show
 		 *the tree*/
 		postOrder(root.left);
 		postOrder(root.right);
-		System.out.println(root.data+" ");
+		System.out.print(root.data+" ");
 	}
-	
-	
+
+
 	/*This is the main function*/
-	
+
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
-		
+
 		Node root = createTree();
+        System.out.println("Inorder is: ");
+        inOrder(root);
+        System.out.println("Preorder is: ");
+        preOrder(root);
+        System.out.println("Postorder is: ");
+        postOrder(root);
+
 	}
-	
-	
-	
+
+
+
 }
