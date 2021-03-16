efe3bbbb596a6a65e65e14528296839a1a8ac120
= <kaiwalyakoparkar@gmail.com>
2021-03-16 17:42:36 +0530

    New approach for solving tried, still wasn't able to solve

--- ProblemSets/Strings/LongestPalindromicSubstring/Solution.java
+++ ProblemSets/Strings/LongestPalindromicSubstring/Solution.java
@@ -2,15 +2,23 @@ package LongestPalindromicSubstring;
 import java.util.*;
 
 public class Solution{
-    //This will find the answer through bruteforce
-    public static boolean isPalindrome(String subString){
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String q = sc.nextLine();//for example string is aaaabbaa
+
+        String bruteCount = bruteforceApproach(q.toCharArray());//O(N^4) complexity
+        System.out.println("Solution through brute force is : " + bruteCount);
+    }
+
+    public static boolean isPalindrome(char sub[]){
         boolean flag = true;
 
         int i = 0;
-        int j = subString.length();
+        int j = sub.length-1;
 
         while(i <= j){
-            if(subString.charAt(i) != subString.charAt(j)){
+            if(sub[i] != sub[j]){
                 return false;
             }
             j--;
@@ -19,35 +27,20 @@ public class Solution{
 
         return flag;
     }
-    public static String bruteforceApproach(String q){
-        String finString = "&";
-        for(int i = 0; i < q.length()-1; i++){
-            for(int j = 1; j < q.length(); j++){
-                String s = q.substring(i,j);
-                boolean pali = isPalindrome(s);
-                if(pali == true && s.length() > finString.length()){
-                    finString = s;
+
+    public static String bruteforceApproach(char q[]){
+        String finString = "";
+        for(int i = 0; i < q.length-1; i++){
+            for(int j = 1; j < q.length; j++){
+                if(isPalindrome(Arrays.copyOfRange(q,i,j))==true && j - i > finString.length()){
+                    int k = i;
+                    while(k <= j){
+                        finString = finString + q[k];
+                        k++;
+                    }
                 }
             }
         }
         return finString;
-    }
-    // public static String betterCount(String q){
-
-    // }
-    // public static String betterCount(String q){
-
-    // }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        String q = sc.nextLine();//for example string is aaaabbaa
-
-        // String bruteCount = bruteforceApproach(q);//O(N^4) complexity
-        // String betterCount = betterCount(q);//O(N^2) complexity
-        // String bestCount = bestCount(q);//O(N) complexity
-
-        System.out.println("Solution through brute force is : " + bruteforceApproach(q));
-        // System.out.println("Solution through better approach is : " + betterCount);
-        // System.out.println("Solution through best approach is : " + bestCount);
     }
 }
