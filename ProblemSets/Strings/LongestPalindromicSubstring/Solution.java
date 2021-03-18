39df8a452d6b6f8d9a59f05d09a4b6162c1f099b
= <kaiwalyakoparkar@gmail.com>
2021-03-18 19:14:00 +0530

    Longest Palindromic string solved

--- ProblemSets/Strings/LongestPalindromicSubstring/Solution.java
+++ ProblemSets/Strings/LongestPalindromicSubstring/Solution.java
@@ -3,7 +3,67 @@ import java.util.*;
 
 public class Solution{
 
-    static void printSubStr(String str, int low, int high) {
+    static void printOptimal(String str, int low, int high) {
+        System.out.println(str.substring(low, high + 1));
+    }
+
+    // This function prints the longest
+    // palindrome substring of str[].
+    // It also returns the length of the
+    // longest palindrome
+    static void optimal(String str)
+    {
+        // get length of input string
+        int n = str.length();
+ 
+        // table[i][j] will be false if
+        // substring str[i..j] is not palindrome.
+        // Else table[i][j] will be true
+        boolean table[][] = new boolean[n][n];
+ 
+        // All substrings of length 1 are palindromes
+        int maxLength = 1;
+        for (int i = 0; i < n; ++i)
+            table[i][i] = true;
+ 
+        // check for sub-string of length 2.
+        int start = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                table[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+ 
+        // Check for lengths greater than 2.
+        // k is length of substring
+        for (int k = 3; k <= n; ++k) {
+ 
+            // Fix the starting index
+            for (int i = 0; i < n - k + 1; ++i) {
+                // Get the ending index of substring from
+                // starting index i and length k
+                int j = i + k - 1;
+ 
+                // checking for sub-string from ith index to
+                // jth index iff str.charAt(i+1) to
+                // str.charAt(j-1) is a palindrome
+                if (table[i + 1][j - 1]
+                    && str.charAt(i) == str.charAt(j)) {
+                    table[i][j] = true;
+ 
+                    if (k > maxLength) {
+                        start = i;
+                        maxLength = k;
+                    }
+                }
+            }
+        }
+        printOptimal(str, start,start + maxLength - 1);
+    }
+
+    static void printBruteForce(String str, int low, int high) {
         for (int i = low; i <= high; ++i)
             System.out.print(str.charAt(i));
     }
@@ -38,9 +98,7 @@ public class Solution{
             }
         }
 
-        printSubStr(str, start, start + maxLength - 1);
-
-        // return length of LPS
+        printBruteForce(str, start, start + maxLength - 1);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -54,9 +112,13 @@ public class Solution{
          * Constantly rotate the 1pointer and 2pointer and check if the string is palindrome
          * If it is palindrome and the size of current string is bigger than than length string then replace the length string with current string.
         */
+        System.out.println("Answer through bruteforce is : ");
         bruteForce(s);
 
+        System.out.println();
         //This is optimized approach
-        
+        System.out.println("Answer through optimal is : " );
+        optimal(s);
+
     }
 }
