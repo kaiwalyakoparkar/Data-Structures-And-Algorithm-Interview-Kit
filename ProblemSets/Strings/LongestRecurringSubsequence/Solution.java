7b8d3d05598b87f6dc5a2116445598aad8344981
= <kaiwalyakoparkar@gmail.com>
2021-03-19 19:55:53 +0530

    Added documentation as well as altered the documentation

--- ProblemSets/Strings/LongestRecurringSubsequence/Solution.java
+++ ProblemSets/Strings/LongestRecurringSubsequence/Solution.java
@@ -1,1 +1,11 @@
-package 
+package LongestRecurringSubsequence;
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        
+    }
+}
